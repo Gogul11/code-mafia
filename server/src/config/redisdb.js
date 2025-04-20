e78bc8cfec4d329de3cce25d08d74f443e0cf6de
Tharun R S <rstharun11@gmail.com>
2025-04-20 19:02:29 +0530

    add reddis  passwd  to connect

--- server/src/config/redisdb.js
+++ server/src/config/redisdb.js
@@ -1,6 +1,11 @@
 import { createClient } from 'redis';
 
-const client = createClient();
+const client = redis.createClient({
+    host: "redis",
+    port: 6379,
+    password: process.env.REDIS_PASSWORD,
+  });
+  
 
 client.on('error', err => console.log('Redis Client Error', err));
 
