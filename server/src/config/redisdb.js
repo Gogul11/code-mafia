7cae3596e8627be978e4388c6ecf3fa17a14efa7
Tharun R S <rstharun11@gmail.com>
2025-04-21 18:28:57 +0530

    fix redis issue

--- server/src/config/redisdb.js
+++ server/src/config/redisdb.js
@@ -1,11 +1,12 @@
 import { createClient } from 'redis';
 
 const client = createClient({
-    host: "redis",
-    port: 6379,
     password: process.env.REDIS_PASSWORD,
-  });
-  
+    socket: {
+        host: "localhost",
+        port: 6379
+    }
+});
 
 client.on('error', err => console.log('Redis Client Error', err));
 
